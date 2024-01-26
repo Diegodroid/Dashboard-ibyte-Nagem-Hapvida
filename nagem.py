427a4bfda1d3360138adb0a09fb5ec58c0c085bd
Diego Barbosa <diegobcarneiro@gmail.com>
2024-01-26 12:44:23 -0300

    Update nagem.py

--- nagem.py
+++ nagem.py
@@ -43,9 +43,7 @@ status_selecionado = st.sidebar.selectbox('Selecione o status:', status_options)
 categoria_selecionada = st.sidebar.selectbox('Selecione a categoria:', categoria_options)
 
 # Adicionar filtro de tamanho para a coluna 'DESCRICAO'
-# Calcular o tamanho máximo da coluna 'DESCRICAO'
-max_size = df_NAGEM['DESCRICAO'].str.len().max()
-min_size, max_size = st.sidebar.slider('Selecione a faixa de tamanho da coluna "DESCRICAO":', 0, max_size, (0, max_size))
+min_size, max_size_selected = st.sidebar.slider('Selecione a faixa de tamanho da coluna "DESCRICAO":', 0, df_NAGEM['DESCRICAO'].str.len().max(), (0, df_NAGEM['DESCRICAO'].str.len().max()))
 # Filter data based on selected options
 df_filtered = df_NAGEM.copy()
 
